8cb59edfa7287c5ba03f0cb0fe48b732cb3f75d4
srushteeM <59560681+srushteeM@users.noreply.github.com>
2022-02-02 14:16:46 +0530

    Add files via upload

---
+++ screens/Meteors.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { Text, View, Alert } from 'react-native';
+import axios from "axios";
+
+export default class MeteorScreen extends Component {
+   
+    render() {
+      
+            return (
+               <View>
+                   <Text>Meteor</Text>
+               </View>
+            )
+        }
+    }
+
